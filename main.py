89d86331b5a294b114507a262789e932c768e7c4
imkostins <imkostin@gmail.com>
2022-06-05 06:45:19 +0200

    *

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # this for python *.py это тесты для питона
-st = input()
-first = st.find('h')
-last = st.rfind('h')
-st = st[:first] + st[(last + 1):]
-print(st)
-print('well done')
+# начинаем работу с фалами
+f = open('xyz.txt','w')
+f.write('Hello NSA')
+f.close()
