6f5a5685d9a10c6ce3b5aa32eafe277d275ac84f
imkostins <imkostin@gmail.com>
2022-06-05 16:03:44 +0200

    только меняю

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # this for python *.py это тесты для питона
 # начинаем работу с файлами
 # Читаю
+import re
+
 with open('xyz.txt', 'r') as file:
     filedata = file.read()
 
 # Меняю
-filedata = filedata.replace('2', '@')
+fd = re.sub(r'1{3,}', '@', filedata)
 
 # Записываю
 with open('xyz.txt', 'w') as file:
-    file.write(filedata)
+    file.write(fd)
